a735697a9cec84fb19f8e415e036ef25ab2d0672
Hippo <hippolyte.pigeat@epfl.ch>
2025-05-25 09:14:37 +0200

    Refactored into multiple files + Gravity

--- src/animation.py
+++ src/animation.py
@@ -1,1 +1,27 @@
-ÿþ
+import pygame
+
+class Ball:
+    def __init__(self, screen_size, ball_size):
+        self.screen_size = screen_size
+        self.ball_size = ball_size
+        self.ball_position = [screen_size[0]//2, screen_size[1]//2]
+        self.ball_velocity = [0, 0]
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.ball_position, self.ball_size)
+        pygame.draw.circle(screen, (255, 0, 0), self.ball_position, self.ball_size-2)
+
+    def moove(self):
+        self.ball_position[0] += self.ball_velocity[0]
+        self.ball_position[1] += self.ball_velocity[1]
+
+
+class Circle:
+    def __init__ (self, screen_size, circle_radius):
+        self.screen_size = screen_size
+        self.circle_radius = circle_radius
+        self.circle_position = [screen_size[0]//2, screen_size[1]//2]
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.circle_position, self.circle_radius)
+        pygame.draw.circle(screen, (0, 0, 0), self.circle_position, self.circle_radius-6)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,33 @@
-ÿþ
+import pygame
+from animation import Ball, Circle
+
+size = (700, 700)
+ball_size = 40
+
+# Initiation
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Ball Animation")
+
+clock = pygame.time.Clock()
+ball = Ball(size, ball_size)
+circle = Circle(size, 300)
+
+# Boucle d'Ã©vÃ©nements pour garder la fenÃªtre ouverte
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    ball.moove()
+    ball.ball_velocity[1] += 0.3
+    screen.fill((0, 0, 0))
+    circle.draw(screen)
+    ball.draw(screen)
+    pygame.display.flip()
+
+    clock.tick(60)
+
+# Quitter Pygame proprement
+pygame.quit()
